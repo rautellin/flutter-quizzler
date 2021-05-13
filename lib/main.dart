630bcc685eb649dff49b7d285d6288377164b51b
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2021-05-13 21:18:45 +0200

    added more classes and functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'questions.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+Questions questionObject = Questions();
 
 void main() => runApp(Quizzler());
 
@@ -26,20 +30,9 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-
-  List<bool> answers = [false, true, true];
-
-  int questionNumber = 0;
-
   void pressedButton() {
     setState(() {
-      bool correctAnswer = answers[questionNumber];
-
+      bool correctAnswer = questionObject.getAnswer();
       if (correctAnswer) {
         scoreKeeper.add(
           Icon(Icons.check, color: Colors.green),
@@ -49,7 +42,20 @@ class _QuizPageState extends State<QuizPage> {
           Icon(Icons.close, color: Colors.red),
         );
       }
-      questionNumber++;
+
+      bool quizIsFinished = questionObject.isFinished();
+      print(quizIsFinished);
+      if (quizIsFinished) {
+        scoreKeeper.clear();
+        questionObject.resetQuestion();
+        Alert(
+                context: context,
+                title: 'Finished!',
+                desc: 'You\'ve reached the end of the quiz.')
+            .show();
+      }
+
+      questionObject.nextQuestion();
     });
   }
 
@@ -65,7 +71,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionObject.getQuestion(),
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
